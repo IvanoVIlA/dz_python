14cca0902c641f61899892057fb0a6b18e4970a6
Ilya <ivanich28hc@gmail.com>
2023-09-26 12:40:49 +0300

    add new file

---
+++ programming.py
@@ -0,0 +1,37 @@
+
+
+# Задача номер 1 для дз по знакомству с питоном
+# n = int(input('n = Введите любое число ')) 
+# count = 0
+# for i in range(n):
+#     x = int(input('x = Введите число от [0 до 1] '))
+#     if x < 1:
+#         count += 1
+#     else:
+#         count = count
+# print (count , ' - число монет, которые нужно перевернуть')   
+
+# Задача 2 для дз по знакомству с языком питон
+s = int(input('s = Введите первое  число '))
+p = int(input('p = Введите второе  число '))
+for x in range(s):
+    for y in range(p):
+        if (x+y==s and x*y==p):
+            print(x, y)
+        
+# Задача 3 для дз по знкомству с питоном
+# n = int(input('n = Введите число ')) 
+# for i in range(n):
+#     summ = 2**i
+#     if summ > n:
+#         break
+#     print('2 в степени = ', summ)
+
+
+
+
+
+
+
+
+
